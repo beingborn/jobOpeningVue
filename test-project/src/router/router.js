6cae377dc7318695fa9bc3ad2e4a165afd850b47
beingborn <dolmin0708@naver.com>
2025-04-01 19:04:49 +0900

    구인등록 마크업

--- test-project/src/router/router.js
+++ test-project/src/router/router.js
@@ -1,6 +1,7 @@
 import {createRouter , createWebHistory} from 'vue-router';
 import Login from '../view/Login.vue'
 import SignUp from '../view/SignUp.vue'
+import JobPost from '../view/JobPost.vue'
 import JobList from '../view/JobList.vue'
 import JobDetail from '../view/JobDetail.vue'
 import UserProfile from '../view/UserProfile.vue'
@@ -13,6 +14,10 @@ const routes = [
     {
         path: '/signup',
         component: SignUp
+    },
+    {
+        path: '/job-post',
+        component: JobPost
     },
     {
         path: '/job-list',
